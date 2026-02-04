264120612ce22d5f15dc46ceaf4c44569f127e21
ArkhamKnightGPC <nazter57@usp.br>
2026-02-04 14:19:17 -0300

    :memo: add tag to setup script (fix LiteX version for project)

--- litex_setup.sh
+++ litex_setup.sh
@@ -1,4 +1,4 @@
 python -m venv venv
 source ./venv/bin/activate
 chmod +x litex_setup.py
-./litex_setup.py --init --install --config=standard
+./litex_setup.py --init --install --config=standard --tag=2025.08
